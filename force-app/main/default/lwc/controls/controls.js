35399afe756eadeacf6363888d2ac52d3ed6a6ce
gsharath999.sk <gsharath999.sk@gmail.com>
2021-08-23 11:16:50 -0500

    NewCOde

--- force-app/main/default/lwc/controls/controls.js
+++ force-app/main/default/lwc/controls/controls.js
@@ -2,7 +2,6 @@ import { LightningElement } from 'lwc';
 
 export default class Controls extends LightningElement {
 
-
     handleAdd() {
         this.dispatchEvent(new CustomEvent('add'));
       }
